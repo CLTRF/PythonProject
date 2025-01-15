9a29257a8cde0d652d1fbfdda43c0d702d2e0bee
CLTRF <clt@gomspace.com>
2025-01-15 17:15:03 +0100

    Creation of an ariticial branch for debug reason

--- BRO/code_py/NF_SBAND.py
+++ BRO/code_py/NF_SBAND.py
@@ -261,7 +261,7 @@ def _NOISE_FIGURE_MEASURMENTS(_str_freq_MHz, _stop_freq_MHz, _IF_BW_MHz, _Nb_Poi
 
     return (True, csv_file)
 
-def _NOISE_FIGURE_MEASURMENTS_X_BAND2(_str_freq_MHz, _stop_freq_MHz, _IF_BW_MHz, _Nb_Points_Sc, _ENR_dB, _frequency_MHz, _span_MHz, _LNA_no, _LNA_serial_number):
+def _NOISE_FIGURE_MEASURMENTS_X_BAND2():
 
     """
    this function measures Noise Figure of the BRO S-BAND LNA using N9000A Spectrum Analyzer.
@@ -297,49 +297,42 @@ def _NOISE_FIGURE_MEASURMENTS_X_BAND2(_str_freq_MHz, _stop_freq_MHz, _IF_BW_MHz,
     from gs_instrument import InstrumentDummy
     from gs_instrument import CsvWriter
 
-    # initalization of instruments
-    #N9000b = N9000A.N9000(N9000a_host)
-    _N9000a=spectrum_analyzer.KeysightCXA(N9000a_host)
-    #SMU200b = SMU200.SMU200(SMU200_host)
+    # Legacy from Max
+    # setup1 P47-10
+    LO_freq = 3365  # error of pll setting #
+    # IF frequnecy
+    IF_freq = 3022.5
+    time_nop = 5
+    if_power = -20
+    myoffset = 17  # need to be specified
+    mod_choice = 0
+    # RF frequnecy
+    RF_range = [9345, 9450]  # MHz
+    IF_range = [3295, 3405]
+    LO = 6045
+    cal = True
 
-
-    _str_freq_MHz   =   3295
-    _stop_freq_MHz  =   3405
-    _IF_BW_MHz      =   2
-    _Nb_Points_Sc   =   40
-    _ENR_dB         =   6
-    _Frequency_Hz           =   1000000000.0 # 1 GHz
-    _Span_Hz                =   25000000.0 #25 MHz
-    _Rbw_Hz                 =   10
-    _Plevel_dB              =   -50
-    _Sweep_points           =   1001
-    _Attenuation_dB         =   0
-    _Detector_Positive_Peak =   True
-    _path_to_usb_drive      =   "F:\\LNA1\\"
-    _Path_to_screen_shot    =   'F:\\'
+    _str_freq_MHz       =   IF_range[0]
+    _stop_freq_MHz      =   IF_range[1]
+    _IF_BW_MHz = 2
+    _Nb_Points_Sc = 40
+    _ENR_dB = 6
+    _Frequency_Hz = 1000000000.0  # 1 GHz
+    _Span_Hz = 25000000.0  # 25 MHz
+    _Rbw_Hz = _IF_BW_MHz * 1e6
+    _Plevel_dB = -20
+    _Sweep_points = 1001
+    _Attenuation_dB = 0
+    _Detector_Positive_Peak = True
+    _path_to_usb_drive      = 'F:\\LNB1\\'
+    _Path_to_screen_shot    = 'F:\\LNB1\\'
     csv_file = ""
-
-    #required setup
-    #mcu
-    #spectrum analyzer N9000A
-    #if generator
-
-
-    #P47-60
-    #RF freq 8250 fixed
-    #IF power fixed
-    #modcon vary
-    #plot spur levels as time20.0	30.0
-    #
-    #20.0	30.0
-    #P47-50
-    #RF freq 8250 fixed
-    #IF power fixed
-    #plot spur levels as time
 
     # local variables Initialisation
     _str_IP_spectrum_analyzer = "10.0.9.212"
     _N9000a=spectrum_analyzer.KeysightCXA(_str_IP_spectrum_analyzer)
+
+    #_N9000a.reset()
 
     # RF frequency
     RF_range=[_str_freq_MHz, _stop_freq_MHz] #MHz
@@ -361,7 +354,7 @@ def _NOISE_FIGURE_MEASURMENTS_X_BAND2(_str_freq_MHz, _stop_freq_MHz, _IF_BW_MHz,
     _N9000a.set_attenuation_dB(_Attenuation_dB)
 
     if _cal==True:
-        _N9000a.NF_meas(RF_range[0], RF_range[1], 2, 8, 41)
+        _N9000a.NF_meas(IF_range[0], IF_range[1], 2, 8, 41)
         #temp_N9000A.NF_meas(_N9000a,RF_range[0],RF_range[1],2,8,41)
         print("Do calibration, hit enter when ready to capture data ")
 
@@ -369,32 +362,25 @@ def _NOISE_FIGURE_MEASURMENTS_X_BAND2(_str_freq_MHz, _stop_freq_MHz, _IF_BW_MHz,
 
         serial_number_count = 0
 
-        for _LNB_no in range(0, 4):
+        for _LNB_no in range(1, 5, 1):
             print("Connect cable to LNB{0} to Do measurements and hit ready to capture data ".format(_LNB_no))
             line = input()
             for _LNB_level_2 in range(1, 5, 1):
-                _LNB_number = _LNB_no + 1
 
-            print("#1.Connect cable to LNA{0} to Do measurements and hit ready to capture data ".format(_LNA_no))
+                print("#1.Connect cable to Branche{0} to Do measurements and hit ready to capture data ".format(_LNB_level_2))
                 line = input()
-                print("#2.Connect cable to LNA{0} to Do measurements and hit ready to capture data ".format(_LNA_no))
-                line = input()
-                #time.sleep(5)
+                time.sleep(5)
 
                 _now = datetime.datetime.now()
                 _now_string = _now.strftime("%d%m%y_%H%M%S")
 
-                _file_name_for_saving = "H:\\DATA_WARE_HOUSE\\data\\" + "LNB" + str(_LNB_number) + "\\BRANCHE" + str(_LNB_level_2) + "\\NF\\Screenshot.png"
-                xxx = "H:\\DATA_WARE_HOUSE\\data\\" + "LNB" + str(_LNB_number) + "\\BRANCHE" + str(_LNB_level_2) + "\\NF\\" + "NF.csv".format(_LNB_number)
+                _file_name_for_saving = "F:\\" + "LNB" + str(_LNB_no) + "\\BRANCHE" + str(_LNB_level_2) + "\\NF\\" + "Screenshot.png"
+                ##xxx = "F:/" + "LNB" + str(_LNB_no) + "/BRANCHE" + str(_LNB_level_2) + "/NF/" + "NF.csv".format(_LNB_no)
+                xxx = "H:\\DATA_WARE_HOUSE\\data\\" + "LNB" + str(_LNB_no) + "\\BRANCHE" + str(_LNB_level_2) + "\\" + "NF.csv".format(_LNB_no)
                 print(_file_name_for_saving)
 
                 _N9000a.save_screenshot_png(_file_name_for_saving)
-
-
                 NF, Gain = list(_N9000a.get_NF())
-    #xxx = "F:\\LNA"+str(_LNA_no)+"\\NF\\"+"SN"+str(_LNA_serial_number)+"{0}.csv".format(_LNA_no)
-    ##xxx = "H:\\DATA_WARE_HOUSE\\data\\"+"LNA"+str(_LNA_no)+"\\SN"+str(_LNA_serial_number)+"\\"+"{0}.csv".format(_LNA_no)
-                xxx = "H:\\DATA_WARE_HOUSE\\data\\" + "LNA" + str(_LNA_no) + "\\SN" + str(_LNA_serial_number) + "\\" + "NF.csv".format(_LNA_no)
                 print(xxx)
                 csv_file = open(xxx, mode='w')
                 for i in range(0, len(NF)):
@@ -402,4 +388,4 @@ def _NOISE_FIGURE_MEASURMENTS_X_BAND2(_str_freq_MHz, _stop_freq_MHz, _IF_BW_MHz,
                     csv_file.write(data)
                 csv_file.close()
 
-    return (True, csv_file)
+        return (True, csv_file)
--- BRO/code_py/SPURIOUS_SBAND.py
+++ BRO/code_py/SPURIOUS_SBAND.py
@@ -103,3 +103,110 @@ def _SPURIOUS_MEASURMENTS(pdiv,ref,V_bw,R_bw,avg,Stop_freq,Start_freq,C_freq,poi
     ## _N9000a.save_screenshot_png('F:/FOU/')
 
     return(True, _file_name_for_saving)
+
+
+def _SPURIOUS_MEASURMENTS_X_BAND(pdiv,ref,V_bw,R_bw,avg,Stop_freq,Start_freq,C_freq,points,offset,lim,_LNB_number,_Branche_number,_str_IP_spectrum_analyzer):
+
+    """
+   this function measures Noise Figure of the BRO S-BAND LNA using N9000A Spectrum Analyzer.
+   it passes:
+   start frequency in MHz
+   stop frequency in MHz
+   Spectrum Analyzer Intermediate Frequency in MHz
+   Number of points in scalar unit
+   ENR in dB
+    """
+
+    import sys,os
+
+#sys.path.insert(1, '/home/max/python/myimport')
+    sys.path.insert(1, 'C:/Users/CLT/PycharmProjects/PythonProject/BRO/Include')
+
+    import csv
+    import copy
+    import logging
+    import time
+    from math import inf
+    from types import SimpleNamespace
+    import numpy as np
+    import gs_instrument
+    from gs_instrument import spectrum_analyzer
+
+    import numpy as np
+    #import SMU200
+    import time
+    import json
+    import datetime
+    import select
+    from gs_instrument import InstrumentDummy
+    from gs_instrument import CsvWriter
+
+## Test Spec
+
+
+#    _str_freq_MHz   =   2000
+#    _stop_freq_MHz  =   3000
+#    _IF_BW_MHz      =   2
+#    _Nb_Points_Sc   =   40
+#    _ENR_dB         =   6
+#   _Frequency_Hz           =   1000000000.0 # 1 GHz
+#    _Span_Hz                =   25000000.0 #25 MHz
+#    _Rbw_Hz                 =   10
+#    _Plevel_dB              =   10
+#    _Sweep_points           =   1001
+#    _Attenuation_dB         =   0
+
+    pdiv = 10
+    ref = 0
+    V_bw = 0
+    R_bw = 30
+    avg = 10
+    Stop_freq = 26000
+    Start_freq = 2600
+    C_freq = 5000
+    points = 801
+    offset = -50
+    _lim = -50
+    _data = []
+    _Detector_Positive_Peak =   True
+    _Path_to_screen_shot    =   'F:\\'
+    _file_name              =   'LNB1'
+    _format_file_screen     =   'PNG'
+
+    _str_freq_MHz   =   Start_freq
+    _stop_freq_MHz  =   Stop_freq
+    _IF_BW_MHz      =   2
+    _Nb_Points_Sc   =   points
+    _ENR_dB         =   6
+    _Frequency_Hz           =   1000000000.0 # 1 GHz
+    _Span_Hz                =   25000000.0 #25 MHz
+    _Rbw_Hz                 =   10
+    _Plevel_dB              =   10
+    _Sweep_points           =   1001
+    _Attenuation_dB         =   0
+
+
+    # local variables Initialisation
+    _str_IP_spectrum_analyzer = "10.0.9.212"
+    _N9000a=spectrum_analyzer.KeysightCXA(_str_IP_spectrum_analyzer)
+    _N9000a.spur_subband_BRO(offset)
+    _N9000a.Spurious_BRO(pdiv, ref, V_bw, R_bw, avg, Stop_freq, Start_freq, C_freq, points, offset)
+
+    #_SPURIOUS_passed_status, _file_name_Spurious = Spurious_BRO(self, pdiv, ref, V_bw, R_bw, avg, Stop_freq, Start_freq,
+    #                                                            C_freq, points, offset)
+
+    #############################################################
+
+    time.sleep(5)
+    _now = datetime.datetime.now()
+    _now_string = _now.strftime("%d%m%y_%H%M%S")
+    #_now_string_time = _now_time.strftime("%H%M%S")
+    #_now_string_date = _now_string_date + _now_string_time
+
+    #_file_name_for_saving = "H:\\DATA_WARE_HOUSE\\data\\"+"LNA"+str(_LNA_number)+"SN"+str(_serial_number)+"\\" + "_now_string_Spurious+Screenshot.png"
+    _file_name_for_saving = "F:\\LNB" + str(_LNB_number) + "\\" + _now_string + "\\Spurious\\" + "Screenshot_LNA"+str(_LNB_number)+"_"+str(_Branche_number)+".png"
+    _N9000a.save_screenshot_png(_file_name_for_saving)
+
+    ## _N9000a.save_screenshot_png('F:/FOU/')
+
+    return(True, _file_name_for_saving)
